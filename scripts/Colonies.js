a14c6c76edcedce0ecc83760e5406ce2dae747a1
Caleb Ford <caleb.ford.sto@gmail.com>
2024-06-27 15:54:38 -0500

    initial logic

--- scripts/Colonies.js
+++ scripts/Colonies.js
@@ -3,8 +3,11 @@
         //import transient state
 //display the corresponding colony
     //find() array method on colonies data to find match between governor and colony
+    //create header html el for colonies.name
 //and that colony's current inventory. 
-        
-//Need to reference the colonyId in some form of transient state.
+        //fetch colonyMinerals
+        //filter() colonyMinerals.colonyId === colony.id
+        //create html el for each colonyMineral with amount and name listed
 
+  
 //Create css tags for the section housing this data.
