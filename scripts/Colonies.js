381d5d04d81b05112fc784e73afb434c79089fdf
Caleb Ford <caleb.ford.sto@gmail.com>
2024-06-27 15:37:43 -0500

    disregard

--- scripts/Colonies.js
+++ scripts/Colonies.js
@@ -1,7 +1,10 @@
 //look at the chosen governor
     //need transient state getter
+        //import transient state
 //display the corresponding colony
+    //find() array method on colonies data to find match between governor and colony
 //and that colony's current inventory. 
+        
 //Need to reference the colonyId in some form of transient state.
 
 //Create css tags for the section housing this data.
