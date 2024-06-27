27a2bdf3984f88afa0b3745595b0b7aa1b0b85ee
James Campbell <jamesrcampbell.obb@gmail.com>
2024-06-27 15:02:03 -0500

    tried modifying the function

--- scripts/MiningFacilities.js
+++ scripts/MiningFacilities.js
@@ -1,22 +1,26 @@
 const miningFacilities = getMiningFacilities()
 
 export const facilities = async () => {
-    let facilitiesHtml = "<div><select id='facilities'><option disabled selected>Select Mining Facility</option>"
-
+    
+let facilitiesHtml = `<div><h2>Facility Minerals for ${facility.name}</h2></div>
+    <div><select id='facilities'><option disabled selected>Select Mining Facility</option>`
     const facilityOptions = miningFacilities.map(
         (facility) => {
+            
             if (facility.isActive === "true") {
                 return`
                     <option value="${facility.id}">${facility.name}</option>
                 `
             }
             return facilityOptions
+
+        
         }
     )
 
-
     facilitiesHtml += facilityOptions.join()
     facilitiesHtml += "</select></div>"
+    
 
     return facilitiesHtml
 }
