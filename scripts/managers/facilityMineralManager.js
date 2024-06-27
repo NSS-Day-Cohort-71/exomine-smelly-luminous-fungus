1bdb8d318845b874cd55e4477a134441dcfcb53b
Caleb Ford <caleb.ford.sto@gmail.com>
2024-06-27 13:51:00 -0500

    rebase commit

--- scripts/managers/facilityMineralManager.js
+++ scripts/managers/facilityMineralManager.js
@@ -1,4 +1,4 @@
 export const getAllFacilityMinerals = async () => {
-    const response = await fetch("http://localhost:8088/Facility_Minerals")
+    const response = await fetch("http://localhost:8088/Facility_Minerals?_expand=mineral&&_expand=facility")
     return await response.json()
 }
