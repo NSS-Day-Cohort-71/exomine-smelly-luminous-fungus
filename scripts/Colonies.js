6bb35b347b89a90e8b9dfe2a52ba9b5123467bb6
Caleb Ford <caleb.ford.sto@gmail.com>
2024-06-27 15:13:48 -0500

    initial commit

---
+++ scripts/Colonies.js
@@ -0,0 +1,7 @@
+//look at the chosen governor
+    //need transient state getter
+//display the corresponding colony
+//and that colony's current inventory. 
+//Need to reference the colonyId in some form of transient state.
+
+//Create css tags for the section housing this data.
