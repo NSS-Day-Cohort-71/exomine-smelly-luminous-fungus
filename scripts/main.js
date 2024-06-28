c8a9c1369cc288be02f69cefa951b6b9bf6b7088
James Campbell <jamesrcampbell.obb@gmail.com>
2024-06-28 13:11:40 -0500

    added css and changed up certain code to facilitate it

--- scripts/main.js
+++ scripts/main.js
@@ -10,16 +10,28 @@ const render = async () => {
 const governorHTML = await GovernorsList();
 const facilitiesHTML = await facilities()
 const colonyHTML = await Colonies()
-    const composedHTML =`
-    ${governorHTML}
-    <article class="facilities>
-        <section class="facilities__options">
+const mineralsHTML = `<div><h2>Facility Minerals for Place Holder</h2></div>*Place miner html variable here*`;
+
+const composedHTML = `
+  <div class="flex-container">
+    <div class="left-panel">
+      <section class="governor-section">
+        ${governorHTML}
+      </section>
+      <section class="facilities-section">
         ${facilitiesHTML}
-        </section></article>
-        <article class="colony">
+      </section>
+      <section class="minerals-section">
+        ${mineralsHTML}
+      </section>
+    </div>
+    <div class="right-panel">
+      <section class="colony">
         ${colonyHTML}
-        </article>
-    `
+      </section>
+    </div>
+  </div>
+`
 
     container.innerHTML = composedHTML
 }
