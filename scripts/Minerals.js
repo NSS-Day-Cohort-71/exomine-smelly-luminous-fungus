842d5374c27111aecdf2b25fa5ce487dd0fef811
Zachary Thacker <zthacker@outlook.com>
2024-07-01 10:24:39 -0500

    got radio buttons workin

--- scripts/Minerals.js
+++ scripts/Minerals.js
@@ -1,40 +1,39 @@
-//have to get minerals from mining facility
+import { render } from "./main.js";
 import { getAllFacilityMinerals } from "./managers/facilityMineralManager.js";
-//import state
-import { state } from "./TransientState.js";
-//display minerals available to colony to purchase
-const facilityMineralList = await getAllFacilityMinerals();
-const facilityId = state.facilityId;
+import { setSelectedMineralId, state } from "./TransientState.js";
+
+// Display minerals available to colony to purchase
 export const MineralsForSale = async () => {
+  const facilityMineralList = await getAllFacilityMinerals();
+  const facilityId = state.facilityId;
+
   const filteredList = facilityMineralList.filter(
     (mineral) => facilityId === mineral.facility.id
   );
+
   return filteredList
     .map((mineral) => {
-      return `<radio id="${mineral.id} /> ${mineral.quantity} ${mineral.mineral.name}`;
+      if (mineral.quantity <= 0) {
+        return `<div><section id="${mineral.id}" name="mineral" value="${mineral.id}" /> ${mineral.quantity} ${mineral.mineral.name}</div>`;
+      }
+      return `<div><input type="radio" id="${mineral.id}" name="mineral" value="${mineral.id}" /> ${mineral.quantity} ${mineral.mineral.name}</div>`;
     })
     .join("");
 };
 
-// return facilityMineralList.map((mineral) => {
-//   mineral.find((facility) => facility.facility.id === facilityId);
-// });
-// export const displayInventory = async () => {
-//   const inventory = await MineralsForSale();
-
-//   inventory.forEach((item) => {
-//     //each options should  have a radio button
-//     return `<div>
-//     <radio id="${item.id} /> ${item.quantity} ${item.mineral.name}
-//     </div>
-//     `;
-//   });
-// };
-
-//if inventory = 0 then no radio button
+// If inventory = 0 then no radio button
 export const mineralsListHTML = async () => {
   let mineralsHTML = `<form id="minerals">`;
   mineralsHTML += await MineralsForSale();
   mineralsHTML += `</form>`;
   return mineralsHTML;
 };
+
+// Update state with selected mineral
+const handleMineralSelection = (e) => {
+  if (e.target.name === "mineral") {
+    setSelectedMineralId(parseInt(e.target.value));
+  }
+};
+
+document.addEventListener("change", handleMineralSelection);
