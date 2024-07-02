f7bdad1651705026ba2bb1d6d28f13ff2c6e74ed
Caleb Ford <caleb.ford.sto@gmail.com>
2024-07-02 10:11:59 -0500

    refactored gov function to default to chosen gov

--- scripts/Governors.js
+++ scripts/Governors.js
@@ -1,20 +1,23 @@
 //get governors with manager function
 
-import { setGovernorId } from "./TransientState.js";
+import { getState, setGovernorId } from "./TransientState.js";
 import { render } from "./main.js";
 import { getAllGovernors } from "./managers/governorManager.js";
 
 //make a dropdown for only active governors
 export const GovernorsList = async () => {
   const governors = await getAllGovernors();
+  const currentState = getState()
   let governorHTML = `<label for="GovernorsList">Choose a Governor</label>
     	                <select id="GovernorsList">
                         <option disabled selected>Select Governor</option>`;
   const governorsMap = governors.map((governor) => {
     if (governor.isActive === true) {
-      return `<option value="${governor.colonyId}">${governor.name}</option>`;
-    }
-  });
+      if(governor.id === currentState.governorId){
+      return `<option value="${governor.Id}" selected>${governor.name}</option>`;
+      } else {
+        return `<option value="${governor.Id}">${governor.name}</option>`;
+}}});
   governorHTML += governorsMap.join("");
   governorHTML += `</select>`;
 
