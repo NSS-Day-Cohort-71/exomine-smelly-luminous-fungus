13e6ff22919618e04c12de3fdc85c0c359939168
Caleb Ford <caleb.ford.sto@gmail.com>
2024-07-02 11:33:22 -0500

    fixed colony display

--- scripts/Colonies.js
+++ scripts/Colonies.js
@@ -1,14 +1,17 @@
 import { getState, setColonyId } from "./TransientState.js";
 import { getAllColonies } from "./managers/colonyManager.js";
 import { getAllColonyMinerals } from "./managers/colonyMineralManager.js";
+import { getAllGovernors } from "./managers/governorManager.js";
 //look at the chosen governor
 export const Colonies = async () => {
   const currentState = getState();
   if (currentState.governorId != 0) {
     const allColonies = await getAllColonies();
+    const allGovernors = await getAllGovernors();
     const allColonyMinerals = await getAllColonyMinerals();
+    const currentGovernor = allGovernors.find(gov => currentState.governorId === gov.id)
     const assignedColony = allColonies.find(
-      (colony) => currentState.governorId === colony.id
+      (colony) => currentGovernor.colonyId === colony.id
     );
     setColonyId(assignedColony.id)
     const colonyInventory = allColonyMinerals.filter(
